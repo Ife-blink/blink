fdd74a52a9ca56e50b8922745b55d8b4f74277da
Ife Asiadiachi <iasiadiachi@gmail.com>
2022-07-16 22:43:28 +0000

    auth

--- firebase.js
+++ firebase.js
@@ -10,6 +10,7 @@ import 'firebase/compat/firestore';
 //import * as FirebaseCore from 'expo-firebase-core';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+//htttp
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
